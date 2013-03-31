e5bdeb20674ccb14e3390d4fad575fd99b6ba8a3
Nícolas Iensen <nicolas@engage.is>
2013-03-31 09:10:59 -0300

    Add validations

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,5 @@
 class Problem < ActiveRecord::Base
   attr_accessible :description, :ideas_deadline, :image, :title, :voting_deadline
+  validates :description, :image, :title, :presence => true
   has_many :ideas
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :image, :last_name
+  validates :email, :first_name, :last_name, :presence => true
+  validates :email, :uniqueness => true
 end
