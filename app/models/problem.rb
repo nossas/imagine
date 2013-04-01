66351379d5d228c0805d19026f4914de7046cae4
Nícolas Iensen <nicolas@engage.is>
2013-04-01 16:43:31 -0300

    Add validation for user id

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,6 @@
 class Problem < ActiveRecord::Base
-  attr_accessible :description, :ideas_deadline, :image, :title, :voting_deadline
-  validates :description, :image, :title, :presence => true
+  attr_accessible :description, :ideas_deadline, :image, :title, :voting_deadline, :user_id
+  validates :description, :title, :user_id, :presence => true
   has_many :ideas
   belongs_to :user
 end
