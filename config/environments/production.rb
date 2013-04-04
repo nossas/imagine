b4a47c42ea051b3c83bdebe08d0e34b9a0d02fbb
Nícolas Iensen <nicolas@engage.is>
2013-04-04 15:18:08 -0300

    Add GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Imagine::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  GA.tracker = "UA-26278513-13"
 end
