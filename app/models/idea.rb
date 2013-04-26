b33942ffe06d5a3347bd16b4664ac49f4f56114c
Nícolas Iensen <nicolas@engage.is>
2013-04-26 16:42:44 -0300

    Add deadline validation to idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ActiveRecord::Base
   attr_accessible :description, :problem_id, :title, :user_id
   validates :description, :problem_id, :title, :user_id, :presence => true
+  validate :ideas_submission_deadline, :on => :create
   belongs_to :problem
   belongs_to :user
   has_many :votes
@@ -18,5 +19,11 @@ class Idea < ActiveRecord::Base
     google_map(:width => "100%")
     link :target => "_blank"
     redcarpet { Redcarpet::Markdown.new(Redcarpet::Render::HTML, :no_intra_emphasis => true) }
+  end
+
+  private
+
+  def ideas_submission_deadline
+    errors.add(:ideas_submission_deadline, "") if Problem.find(self.problem_id).contribution_closed?
   end
 end
