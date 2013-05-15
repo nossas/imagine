8ea9629ff1f8376a4433a77595e247a09f46a989
Nícolas Iensen <nicolas@engage.is>
2013-05-15 09:15:51 -0300

    Fix carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,11 @@
 CarrierWave.configure do |config|
+  config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV["AWS_ID"],
     :aws_secret_access_key  => ENV["AWS_SECRET"],
   }
-  config.fog_directory  = 'imaginee' 
+  config.fog_directory  = ENV["AWS_BUCKET"]
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 
   if Rails.env.test? or Rails.env.cucumber?
