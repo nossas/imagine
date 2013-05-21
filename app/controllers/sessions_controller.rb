452d25d6124f7bb5181de7a05a2ebddcfb07b9f5
Nícolas Iensen <nicolas@engage.is>
2013-05-21 11:55:14 -0300

    Update user information when login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,27 @@ class SessionsController < ApplicationController
 
   def create
     hash = request.env['omniauth.auth']
-    session[:user_id] = User.find_or_create_by_uid(
-      hash[:uid],
-      :uid        => hash[:uid],
-      :email      => hash[:info][:email],
-      :first_name => hash[:info][:first_name], 
-      :last_name  => hash[:info][:last_name],
-      :token      => hash[:credentials][:token],
-      :image      => hash[:info][:image]).id
+    user = User.find_by_uid(hash[:uid])
+    if user
+      user.update_attributes(
+        :uid        => hash[:uid],
+        :email      => hash[:info][:email],
+        :first_name => hash[:info][:first_name], 
+        :last_name  => hash[:info][:last_name],
+        :token      => hash[:credentials][:token],
+        :image      => hash[:info][:image]
+      )
+    else
+       user = User.create(
+        :uid        => hash[:uid],
+        :email      => hash[:info][:email],
+        :first_name => hash[:info][:first_name], 
+        :last_name  => hash[:info][:last_name],
+        :token      => hash[:credentials][:token],
+        :image      => hash[:info][:image]
+      )   
+    end
+    session[:user_id] = user.id
     if session[:idea_id]
       idea = Idea.find(session.delete(:idea_id))
       redirect_to create_from_session_problem_idea_votes_path(problem_id: idea.problem.id, idea_id: idea.id)
