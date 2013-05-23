f6f62cf497ce74fd65957727c74bdf4bec7aab16
Nícolas Iensen <nicolas@engage.is>
2013-05-23 17:19:31 -0300

    Fix problem images

--- app/uploaders/problem_image_uploader.rb
+++ app/uploaders/problem_image_uploader.rb
@@ -37,7 +37,7 @@ class ProblemImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [150, 150]
+    process :resize_to_fill => [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
