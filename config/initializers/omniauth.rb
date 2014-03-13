13015e57f1d5a347fae20f812f70c157299f1db3
Nícolas Iensen <nicolas@engage.is>
2014-03-13 11:59:21 -0300

    Change omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: "email, manage_pages, publish_stream"
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: "email"
 end
